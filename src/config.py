f11b6eab31b3c703d88e3dc36cbbd44c90387897
ti-ecoparque <ti.cascavel@ecoparque.com.br>
2026-07-17 16:35:19 -0300

    senha

--- src/config.py
+++ src/config.py
@@ -6,22 +6,29 @@ def configurar_layout():
 
 def checar_autenticacao():
     """Valida se o usuário está logado. Se não estiver, exibe a tela de login."""
+
     if "autenticado" not in st.session_state:
         st.session_state.autenticado = False
 
     if not st.session_state.autenticado:
+
         st.title("🔑 Acesso ao Sistema")
-        usuario_correto = st.secrets["credentials"]["username"]
-        senha_correta = st.secrets["credentials"]["password"]
-        
+
         with st.form("form_login"):
+
             usuario_input = st.text_input("Usuário")
             senha_input = st.text_input("Senha", type="password")
             botao_entrar = st.form_submit_button("Entrar")
+
             if botao_entrar:
-                if usuario_input == usuario_correto and senha_input == senha_correta:
+                usuarios = st.secrets["credentials"]
+                if (
+                    usuario_input in usuarios
+                    and usuarios[usuario_input] == senha_input
+                ):
                     st.session_state.autenticado = True
+                    st.session_state.usuario = usuario_input
                     st.rerun()
                 else:
                     st.error("Usuário ou senha incorretos.")
-        st.stop() # Interrompe a execução do restante da página se não logado
+        st.stop()
